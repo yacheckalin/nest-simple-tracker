428e038bf8a7a2119391a80b37c3147517b6dfd7
Yaroslav Chekalin <yaroslav.checkalin@gmail.com>
2023-09-07 12:04:36 +0200

    feat: add unit tests for customer.controller, customers.service

--- core-api/src/customers/customers.controller.spec.ts
+++ core-api/src/customers/customers.controller.spec.ts
@@ -1,18 +1,59 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { CustomersController } from './customers.controller';
+import { CustomersService } from './customers.service';
+import { CreateCustomerDto } from './dto/create-customer.dto';
 
 describe('CustomersController', () => {
   let controller: CustomersController;
+  let modRef: TestingModule;
+  let service: CustomersService;
 
   beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
+    modRef = await Test.createTestingModule({
       controllers: [CustomersController],
+      providers: [
+        {
+          provide: CustomersService,
+          useFactory: () => ({
+            getAllCustomers: jest.fn(() => []),
+            getCustomerById: jest.fn((id: number) => ({
+              id,
+              email: 'some@email.com',
+            })),
+            createCustomer: jest.fn((obj: CreateCustomerDto) => ({
+              id: 1,
+              email: obj.email,
+            })),
+          }),
+        },
+      ],
     }).compile();
 
-    controller = module.get<CustomersController>(CustomersController);
+    controller = modRef.get<CustomersController>(CustomersController);
+    service = modRef.get<CustomersService>(CustomersService);
+  });
+
+  afterEach(async () => {
+    await modRef.close();
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  it('calling getAllCustomers method', () => {
+    controller.getAllCustomers();
+    expect(service.getAllCustomers).toHaveBeenCalled();
+  });
+  it('calling getCustomerById method', () => {
+    controller.getCustomerById(1);
+    expect(service.getCustomerById).toHaveBeenCalled();
+    expect(service.getCustomerById).toHaveBeenCalledWith(1);
+  });
+  it('calling createCustomer method', () => {
+    const dto = new CreateCustomerDto();
+    controller.createCustomer(dto);
+    expect(service.createCustomer).toHaveBeenCalled();
+    expect(service.createCustomer).toHaveBeenCalledWith(dto);
   });
 });
--- core-api/src/customers/customers.service.spec.ts
+++ core-api/src/customers/customers.service.spec.ts
@@ -1,18 +1,59 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { CustomersService } from './customers.service';
+import { CreateCustomerDto } from './dto/create-customer.dto';
+import { CustomersModule } from './customers.module';
 
 describe('CustomersService', () => {
   let service: CustomersService;
+  let modRef: TestingModule;
 
   beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [CustomersService],
+    modRef = await Test.createTestingModule({
+      providers: [
+        {
+          provide: CustomersService,
+          useFactory: () => ({
+            getAllCustomers: jest.fn(() => []),
+            getCustomerById: jest.fn((id: number) => ({
+              id,
+              email: 'some@email.com',
+            })),
+            createCustomer: jest.fn((obj: CreateCustomerDto) => ({
+              id: 1,
+              email: obj.email,
+            })),
+          }),
+        },
+      ],
     }).compile();
 
-    service = module.get<CustomersService>(CustomersService);
+    service = modRef.get<CustomersService>(CustomersService);
+  });
+
+  afterEach(async () => {
+    await modRef.close();
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('should call getAllCustomers method', async () => {
+    const getAllCustomersSpy = jest.spyOn(service, 'getAllCustomers');
+    service.getAllCustomers();
+    expect(getAllCustomersSpy).toHaveBeenCalled();
+  });
+  it('should call getCustomerById method', async () => {
+    const getCustomerByIdSpy = jest.spyOn(service, 'getCustomerById');
+    service.getCustomerById(1);
+    expect(getCustomerByIdSpy).toHaveBeenCalled();
+    expect(getCustomerByIdSpy).toHaveBeenCalledWith(1);
+  });
+  it('should call createCustomer method', async () => {
+    const createCustomerSpy = jest.spyOn(service, 'createCustomer');
+    const dto = new CreateCustomerDto();
+    service.createCustomer(dto);
+    expect(createCustomerSpy).toHaveBeenCalled();
+    expect(createCustomerSpy).toHaveBeenCalledWith(dto);
   });
 });
